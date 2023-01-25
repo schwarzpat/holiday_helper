d6dd6e11c1461e3e81730cef5425b411385e5f87
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-01-25 10:50:35 +0100

    Update holday_helper.R

--- holday_helper.R
+++ holday_helper.R
@@ -3,9 +3,8 @@ library(dplyr)
 # Read the csv file
 holidays_df <- read.csv("generated_holidays.csv")
 
-filter_holidays <- function(holidays_df, country_code) {
-  filtered_df <- filter(holidays_df, country == country_code)
-  
-# Return the vecotr of dates
+filter_holidays <- function(holidays_df, country_code, date_limit = "9999-12-31") {
+  filtered_df <- filter(holidays_df,
+                        country == country_code & ds <= date_limit)
   return(filtered_df$ds)
 }
